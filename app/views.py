75444e52ecd4fd15b8898d809d4b6273d69368fc
Sharon Cherop <sh.cherop@gmail.com>
2021-09-12 11:24:20 +0300

    adding a news html template

--- app/views.py
+++ app/views.py
@@ -9,3 +9,10 @@ def index():
     '''
     message='News Hub'
     return render_template('index.html', message=message)
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+    '''
+    View news page funciton that return news details and its data
+    '''
+    return render_template('news.html', id=news_id)
