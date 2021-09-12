7add2ca0cad5a218bc220f0cf0df69865dbfae53
Sharon Cherop <sh.cherop@gmail.com>
2021-09-12 15:41:05 +0300

    creating process results funciton

--- app/request.py
+++ app/request.py
@@ -10,3 +10,25 @@ api_key=app.config['NEWS_API_KEY']
 
 #getting the news base url
 base_url=app.config['NEWS_API_BASE_URL']
+
+def get_news(category):
+    '''
+    function that gets json response to our url
+    '''
+    get_news_url=base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data=url.read()
+        get_news_response=json.loads(get_news_data)
+
+        news_result=None
+
+        if get_news_response['results']:
+            news_result_list=get_news_response['results']
+            news_result=process_results(news_result_list)
+
+    return news_result
+
+def process_results(news_list):
+    '''
+    Function that processes news results and processes it into a list'''
